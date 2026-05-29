2d6c44d8d6e02e1ff54769f917a30d423e9b9be2
Jpzibarth <joaopedrozibarth@gmail.com>
2026-05-29 11:01:40 -0300

    2

--- script_aluno.js
+++ script_aluno.js
@@ -13,46 +13,33 @@ function calcularCompra() {
 
     
     // REGRA 1: Validação do Cupom de Desconto
-    //O nome do cupom oficial é "PROMO10". 
-    // Use .toUpperCase() para garantir que funcione se o usuário digitar "promo10" ou "Promo10"
-    
-    // Faça um teste condicional (if): se o 'cupomDigitado' for igual ao cupom oficial o valor de desconto será = a 10
-     
-    // DICA: Use a estrutura -> if (variavel.toUpperCase() === "VALOR") { valor da vaiável desconto}
-    
     let desconto = 0;
     
+    if (cupomDigitado.toUpperCase() === "PROMO10") {
+        desconto = 10;
+    }
 
 
     // REGRA 2: Aplicação do Desconto no Valor do Produto
-
-    // Crie uma nova variável chamada 'valorComDesconto'.
-    // Ela deve guardar o resultado de uma conta matemática simples:
-    // O 'valorOriginal' do produto MENOS (-) o 'desconto' que você descobriu na Regra 1.
-    
-    // ESCREVA SEU CÓDIGO AQUI:
-
+    // O 'valorOriginal' do produto MENOS (-) o 'desconto'
+    let valorComDesconto = valorOriginal - desconto;
+  
 
   
     // REGRA 3: Cálculo da Taxa de Frete
-   
-    // Agora crie uma estrutura condicional completa (if / else):
-    // -> SE o 'valorComDesconto' for MAIOR OU IGUAL (>=) a 100, o frete continua 0.
-    // -> SENÃO (else), a variável 'frete' deve receber o valor de 15.
-    //
-    // DICA: Use a estrutura -> if ( ... ) { ... } else { ... }
     let frete = 0;
     
-    // ESCREVA SEU IF / ELSE AQUI:
+    if (valorComDesconto >= 100) {
+        frete = 0;
+    } else {
+        frete = 15;
+    }
 
 
    
     // REGRA 4: Total Geral da Compra
-    // -----------------------------------------------------------------
-    // Crie a última variável chamada 'totalFinal'.
-    // Ela deve guardar a soma (+) do 'valorComDesconto' com a taxa do 'frete'.
-    
-    // ESCREVA SEU CÓDIGO AQUI:
+    // Soma (+) do 'valorComDesconto' com a taxa do 'frete'
+    let totalFinal = valorComDesconto + frete;
 
 
     // =================================================================
@@ -64,7 +51,7 @@ function calcularCompra() {
     painel.innerHTML = `
         <strong>Resumo do Pedido:</strong><br>
         Desconto: R$ ${desconto.toFixed(2)}<br>
-        Frete: R$ ${frete === 0 ? "Grátis" : "R$ " + frete.toFixed(2)}<br>
+        Frete: ${frete === 0 ? "Grátis" : "R$ " + frete.toFixed(2)}<br>
         <strong>Total a pagar: R$ ${totalFinal.toFixed(2)}</strong>
     `;
 }
